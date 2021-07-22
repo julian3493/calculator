cb39deea9bfdb2eaa6d6ef4e2f489205b48364cf
julian3493 <julianramosarevalo@gmail.com>
2021-07-22 16:33:33 -0500

    delete app.css file and delete lines 7-21 in app.js file

--- src/App.js
+++ src/App.js
@@ -4,21 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello World!</p>
       </header>
     </div>
   );
