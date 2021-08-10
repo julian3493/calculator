f903578d3648c595d0bf03d313dcf313d7056e1b
julian3493 <julianramosarevalo@gmail.com>
2021-08-10 18:06:34 -0500

    implement handleclick function in buttonpanel.js

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -6,40 +6,41 @@ function ButtonPanel(props) {
   return (
     <div className="panel">
       <div className="row">
-        <Button name="AC" clickHandler={() => props.clickHandler('AC')} />
-        <Button name="+/-" clickHandler={() => props.clickHandler('+/-')} />
-        <Button name="%" clickHandler={() => props.clickHandler('%')} />
-        <Button name="÷" clickHandler={() => props.clickHandler('÷')} />
+        <Button name="AC" clickHandler={() => handleClick('AC')} />
+        <Button name="+/-" clickHandler={() => handleClick('+/-')} />
+        <Button name="%" clickHandler={() => handleClick('%')} />
+        <Button name="÷" clickHandler={() => handleClick('÷')} />
       </div>
       <div className="row">
-        <Button name="7" clickHandler={() => props.clickHandler('7')} />
-        <Button name="8" clickHandler={() => props.clickHandler('8')} />
-        <Button name="9" clickHandler={() => props.clickHandler('9')} />
-        <Button name="X" clickHandler={() => props.clickHandler('X')} />
+        <Button name="7" clickHandler={() => handleClick('7')} />
+        <Button name="8" clickHandler={() => handleClick('8')} />
+        <Button name="9" clickHandler={() => handleClick('9')} />
+        <Button name="X" clickHandler={() => handleClick('X')} />
       </div>
       <div className="row">
-        <Button name="4" clickHandler={() => props.clickHandler('4')} />
-        <Button name="5" clickHandler={() => props.clickHandler('5')} />
-        <Button name="6" clickHandler={() => props.clickHandler('6')} />
-        <Button name="-" clickHandler={() => props.clickHandler('-')} />
+        <Button name="4" clickHandler={() => handleClick('4')} />
+        <Button name="5" clickHandler={() => handleClick('5')} />
+        <Button name="6" clickHandler={() => handleClick('6')} />
+        <Button name="-" clickHandler={() => handleClick('-')} />
       </div>
       <div className="row">
-        <Button name="1" clickHandler={() => props.clickHandler('1')} />
-        <Button name="2" clickHandler={() => props.clickHandler('2')} />
-        <Button name="3" clickHandler={() => props.clickHandler('3')} />
-        <Button name="+" clickHandler={() => props.clickHandler('+')} />
+        <Button name="1" clickHandler={() => handleClick('1')} />
+        <Button name="2" clickHandler={() => handleClick('2')} />
+        <Button name="3" clickHandler={() => handleClick('3')} />
+        <Button name="+" clickHandler={() => handleClick('+')} />
       </div>
       <div className="row">
-        <Button name="0" clickHandler={() => props.clickHandler('0')} />
-        <Button name="." clickHandler={() => props.clickHandler('.')} />
-        <Button name="=" clickHandler={() => props.clickHandler('=')} />
+        <Button name="0" clickHandler={() => handleClick('0')} />
+        <Button name="." clickHandler={() => handleClick('.')} />
+        <Button name="=" clickHandler={() => handleClick('=')} />
       </div>
     </div>
   );
 }
 
-ButtonPanel.propTypes = {
-  clickHandler: PropTypes.func.isRequired,
-};
-
+function handleClick(btnName) {
+  return(
+    props.clickHandler(btnName)
+  )
+}
 export default ButtonPanel
