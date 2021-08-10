4aa35dc290e44dfa4ef8f62025606ee661e9f57e
julian3493 <julianramosarevalo@gmail.com>
2021-08-10 18:23:49 -0500

    implement handleclick function in button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,10 +5,16 @@ const Button = (props) => {
   const { name } = props;
   return (
     <div>
-      <button type="button" name="button">{name}</button>
+      <button type="button" name="button" onClick={ handleClick(name) }>{name}</button>
     </div>
   );
 };
+
+function handleClick(btnName) {
+  return(
+    props.clickHandler(btnName)
+  )
+}
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
