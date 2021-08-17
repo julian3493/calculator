909732002eda80b70d4e84bb699c7b3c7e0d5b16
julian3493 <julianramosarevalo@gmail.com>
2021-08-17 11:49:37 -0500

    remove comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
