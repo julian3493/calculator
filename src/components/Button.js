26f810bc7713974e150e487351f1709d3a128834
julian3493 <julianramosarevalo@gmail.com>
2021-08-23 18:34:00 -0500

    add style in panel and button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/button.css';
 
 const Button = ({ name, clickHandler }) => {
   const handleClick = (btnName) => {
     clickHandler(btnName);
   };
 
+  const className = (name) => {
+    const ops = ['+', '-', 'X', '÷', '='];
+    if (name === '0') return 'btn btn0';
+    if (ops.includes(name)) return 'btn btnColor';
+    return 'btn';
+  };
+
   return (
-    <div>
-      <button type="button" name="button" onClick={() => handleClick(name)}>{name}</button>
+    <div className={className(name)}>
+      <button className={className(name)} type="button" name="button" onClick={() => handleClick(name)}>{name}</button>
     </div>
   );
 };
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
+import '../styles/panel.css';
 
 function ButtonPanel({ clickHandler }) {
   const handleClick = (btnName) => {
