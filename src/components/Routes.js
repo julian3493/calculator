bdf2611f175053701d409057821ead1cfdfa00e4
julian3493 <julianramosarevalo@gmail.com>
2021-08-30 17:56:11 -0500

    create homepage file

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import home from '../pages/home';
 import App from './App';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={App} />
+      <Route exact path="/" component={home} />
+      <Route exact path="/app" component={App} />
     </Switch>
   </BrowserRouter>
 );
---
+++ src/pages/home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const home = () => (
+  <div className="quote">
+    <h2>Welcome to my page!</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus velit ex, a pellentesque
+      elit lacinia non.Donec in augue id orci molestie efficitur. Sed non mi augue. Curabitur et sem
+      et risus faucibus bibendum et id lorem.Nunc congue fringilla lectus sed placerat. Phasellus
+      bibendum lorem ac semper pharetra. Vestibulum maximus augue vel tortorscelerisque vestibulum.
+      Phasellus tincidunt, nisl in vehicula semper, eros diam accumsan nisl, eget mollis magna sem
+      velquam.Donec ut nibh rhoncus, vehicula odio eget, iaculis odio. Nunc vestibulum elit ligula,
+      mollis venenatis metus lacinia vitae.
+    </p>
+  </div>
+);
+
+export default home;
